d10fd437428def6154957625f3b258f219e87f33
Rui Lopes <rgl@ruilopes.com>
2021-09-25 08:59:22 +0100

    customize windows

--- provision-base.ps1
+++ provision-base.ps1
@@ -1,3 +1,67 @@
+# define the Install-Application function that downloads and unzips an application.
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Install-Application($name, $url, $expectedHash, $expectedHashAlgorithm = 'SHA256') {
+    $localZipPath = "$env:TEMP\$name.zip"
+    (New-Object Net.WebClient).DownloadFile($url, $localZipPath)
+    $actualHash = (Get-FileHash $localZipPath -Algorithm $expectedHashAlgorithm).Hash
+    if ($actualHash -ne $expectedHash) {
+        throw "$name downloaded from $url to $localZipPath has $actualHash hash that does not match the expected $expectedHash"
+    }
+    $destinationPath = Join-Path $env:ProgramFiles $name
+    [IO.Compression.ZipFile]::ExtractToDirectory($localZipPath, $destinationPath)
+}
+
+# disable cortana and web search.
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -Force `
+    | New-ItemProperty -Name AllowCortana -Value 0 `
+    | New-ItemProperty -Name ConnectedSearchUseWeb -Value 0 `
+    | Out-Null
+
+# set keyboard layout.
+# NB you can get the name from the list:
+#      [Globalization.CultureInfo]::GetCultures('InstalledWin32Cultures') | Out-GridView
+Set-WinUserLanguageList pt-PT -Force
+
+# set the date format, number format, etc.
+Set-Culture pt-PT
+
+# set the welcome screen culture and keyboard layout.
+# NB the .DEFAULT key is for the local SYSTEM account (S-1-5-18).
+New-PSDrive HKU -PSProvider Registry -Root HKEY_USERS | Out-Null
+'Control Panel\International','Keyboard Layout' | ForEach-Object {
+    Remove-Item -Path "HKU:\.DEFAULT\$_" -Recurse -Force
+    Copy-Item -Path "HKCU:\$_" -Destination "HKU:\.DEFAULT\$_" -Recurse -Force
+}
+Remove-PSDrive HKU
+
+# set the timezone.
+# use Get-TimeZone -ListAvailable to list the available timezone ids.
+Set-TimeZone -Id 'GMT Standard Time'
+
+# show window content while dragging.
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name DragFullWindows -Value 1
+
+# show hidden files.
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Value 1
+
+# show protected operating system files.
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowSuperHidden -Value 1
+
+# show file extensions.
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0
+
+# display full path in the title bar.
+New-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState -Force `
+    | New-ItemProperty -Name FullPath -Value 1 -PropertyType DWORD `
+    | Out-Null
+
+# set desktop background.
+Copy-Item C:\vagrant\winpe.jpg C:\Windows\Web\Wallpaper\Windows
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper -Value C:\Windows\Web\Wallpaper\Windows\winpe.jpg
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallpaperStyle -Value 0
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name TileWallpaper -Value 0
+Set-ItemProperty -Path 'HKCU:\Control Panel\Colors' -Name Background -Value '1 0 171'
+
 # install tooling.
 choco install -y 7zip
 choco install -y carbon
