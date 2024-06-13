a7a0baf73eccd1da40d4963ff1ddc6646c67a100
Rui Lopes <rgl@ruilopes.com>
2024-06-13 20:32:14 +0100

    upgrade to virtio-win 0.1.248-1

--- provision-winpe-virtio.ps1
+++ provision-winpe-virtio.ps1
@@ -2,7 +2,7 @@
 # see https://github.com/virtio-win/virtio-win-guest-tools-installer
 # see https://github.com/virtio-win/virtio-win-pkg-scripts
 Write-Output 'Adding the virtio drivers...'
-$qemuDriversIsoUrl = 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.229-1/virtio-win-0.1.229.iso'
+$qemuDriversIsoUrl = 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.248-1/virtio-win-0.1.248.iso'
 $qemuDriversIsoPath = "C:\vagrant\tmp\$(Split-Path -Leaf $qemuDriversIsoUrl)"
 $qemuDriversPath = "$env:TEMP\$([IO.Path]::GetFileNameWithoutExtension($qemuDriversIsoUrl))"
 if (!(Test-Path $qemuDriversIsoPath)) {
@@ -15,6 +15,21 @@ if (!(Test-Path $qemuDriversPath)) {
 Get-ChildItem $qemuDriversPath -Include 2k22 -Recurse `
     | Where-Object { Test-Path "$_\amd64" } `
     | ForEach-Object {
-        Write-Output "Adding the $_\amd64 driver..."
-        Add-WindowsDriver -Path $env:WINDOWS_PE_MOUNT_PATH -Driver "$_\amd64"
+        Get-ChildItem "$_\amd64\*.inf" | ForEach-Object {
+            $driverPath = $_.FullName
+            Write-Output "Adding the $driverPath driver..."
+            try {
+                Add-WindowsDriver -Path $env:WINDOWS_PE_MOUNT_PATH -Driver $driverPath
+            } catch {
+                # NB as-of virtio-win 0.1.248, the following drivers fail to be added:
+                #       pvpanic-pci.inf
+                #       smbus.inf
+                # see https://github.com/virtio-win/kvm-guest-drivers-windows/issues/1107
+                if ("$_" -match 'The request is not supported') {
+                    Write-Host "WARNING The driver was not added. Ignoring the known error: $_"
+                    return
+                }
+                throw
+            }
+        }
     }
